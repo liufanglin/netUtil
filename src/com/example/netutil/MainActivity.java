bd604e32be7118dde7caa239889a388b5d414e72
liufanglin <1085873616@qq.com>
2015-11-23 16:42:20 +0800

    网络编程util的最后版本

--- src/com/example/netutil/MainActivity.java
+++ src/com/example/netutil/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.netutil;
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,22 +12,27 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	private TextView tv_show;
 	private Button btn_result;
-	private String type="POST";
-	private String name;
 	protected static final int FAILTURE = 0;	
 	protected static final int SUCCESS=1;
 	private Map<String, String> params=new HashMap<>();
+	private String filepath="file:///android_asset/aaa.png";
+	private String path="http://192.168.216.43:8080/SpringMVC8/user/1/photo";
 	
-	private String path="http://www.weather.com.cn/adat/sk/101110101.html";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		tv_show=(TextView) findViewById(R.id.tv_show);
 		btn_result=(Button) findViewById(R.id.btn_post);
-		//第一个参数是请求头的参数，第二个是上传文件用的，传一个file对象，传空代表我们不传文件,
+		params.put("id", "1");
+		params.put("name", "zhangsan");
+		params.put("type", "POST");
+		params.put("path", path);
+		params.put("isupfile", filepath);
+		
+		//第一个参数是请求的路径,第一个参数是请求头的参数，第二个是上传文件用的，传一个file对象，传空代表我们不传文件,
 		//第三个参数是代表我要发送的请求的类型，第四个是文件参数，第五个是实现的接口回调，通过里面的方法我们拿到返回的结果
-		MyAsynctask task=new MyAsynctask(params,null,type,name,new BackResult() {
+		MyAsynctask task=new MyAsynctask(this,params,new BackResult() {
 			
 			@Override
 			public void BackResult(Boolean flag, String json) {
@@ -38,10 +44,11 @@ public class MainActivity extends Activity {
 					tv_show.setText(json);
 				}else {
 					Log.i("返回", "访问失败"+json);
+					tv_show.setText(json);
 				}
 			}
 		});
-		task.execute(path);	
+		task.execute();	
 
 	}
 
--- src/com/example/netutil/MyAsynctask.java
+++ src/com/example/netutil/MyAsynctask.java
@@ -10,8 +10,12 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 /**
@@ -24,25 +28,26 @@ import android.util.Log;
  */
 public class MyAsynctask extends AsyncTask<String, Integer, String>{
 	private Map<String, String> params;
-	private String type;
 	private BackResult br;
 	protected static final String GET="GET";
 	protected static final String POST="POST";
 	protected static final String SEND="SEND";
-	private File file;
 	private String json;
 	private boolean flag=true;
 	private String TAG="信息打印";
-	private String name;
+	private Context context;
 	
 	//第一个参数是请求头的参数，第二个是上传文件用的，传一个file对象，传空代表我们不传文件,
 	//第三个参数是代表我要发送的请求的类型，第四个是文件参数，第五个是实现的接口回调，通过里面的方法我们拿到返回的结果
-	public MyAsynctask(Map<String, String> params,File file,String type,String name,BackResult br){
+	public MyAsynctask(Context context,Map<String, String> params,BackResult br){
 		this.params=params;
-		this.type=type;
 		this.br=br;
-		this.file=file;
-		this.name=name;
+		this.context=context;
+	}
+	
+	public MyAsynctask(Map<String, String> params,BackResult br){
+		this.params=params;
+		this.br=br;
 	}
 	/**
 	 * 这个方法在调用excute时候调用,用来初始化UI用的
@@ -57,17 +62,38 @@ public class MyAsynctask extends AsyncTask<String, Integer, String>{
 	  */
      @Override  
      protected String doInBackground(String... path) {  
-    	 if(file==null){
-    	 if(type.equals(GET)){
+    	 if(params.get("isupfile")==null){
+    		
+    	 if(params.get("type").equals(GET)){
+    	
  			try {
- 				URL url = new URL(path[0]);
+ 				
+ 				StringBuilder sb = new StringBuilder();  
+ 		        sb.append(params.get("path")).append("?");  
+ 		  	
+ 		        if (params != null && params.size() != 0) {  
+ 		        	Set<String> keys=params.keySet();
+ 		        	Iterator<String> iter=keys.iterator();
+ 		        	while(iter.hasNext()){
+ 		        		String key=iter.next();
+ 		        		if (!key.equals("path")&&!key.equals("type")) {
+ 		        			String value=params.get(key);
+ 		        			sb.append(key).append("=").append(URLEncoder.encode(value, "utf-8"));
+ 		        			sb.append("&");
+						}
+ 		       	}
+ 		            sb.deleteCharAt(sb.length() - 1);  
+ 		            System.out.println(sb.toString()+"--------");
+ 		        }  
+ 				URL url = new URL(sb.toString());
  				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
  				conn.setRequestMethod("GET");
  				conn.setConnectTimeout(3000);
  				int code = conn.getResponseCode();
- 				
+ 				 System.out.println("------------------"+code);
  				if (200 == code) {
  					InputStream is = conn.getInputStream();
+ 					
  					json=StreamTools.readStream(is);
  					flag=true;
  					} else {
@@ -75,25 +101,31 @@ public class MyAsynctask extends AsyncTask<String, Integer, String>{
  						json="请求失败";
  							}
  						} catch (Exception e) {
+ 							flag=false;
+ 							json=e.getMessage();
  							e.printStackTrace();
  						}
  					}
- 	    if (type.equals(POST)) {
-         StringBuilder sb = new StringBuilder();  
+ 	    if (params.get("type").equals(POST)) {
+         StringBuilder sb = new StringBuilder(); 
+         try {
          if(params!=null &params.size()!=0){  
-             for (Map.Entry<String, String> entry : params.entrySet()) {  
-                    try {
- 					sb.append(entry.getKey()).append("=").append(URLEncoder.encode(entry.getValue(), "utf-8"));
-                     sb.append("&");   
-                    } catch (UnsupportedEncodingException e) {
-    					e.printStackTrace();
-    				}  
-             }  
+        	 
+        		Set<String> keys=params.keySet();
+		        	Iterator<String> iter=keys.iterator();
+		        	while(iter.hasNext()){
+		        		String key=iter.next();
+		        		if (!key.equals("path")&&!key.equals("type")) {
+		        			String value=params.get(key);
+		        			sb.append(key).append("=").append(URLEncoder.encode(value, "utf-8"));
+		        			sb.append("&");
+					}
+		       	}
              sb.deleteCharAt(sb.length()-1);  
          }  
          byte[] entity = sb.toString().getBytes();  
-         try {
-          URL url = new URL(path[0]);
+         
+          URL url = new URL(params.get("path"));
           HttpURLConnection conn = (HttpURLConnection) url.openConnection();  
           conn.setConnectTimeout(2000);  
           conn.setRequestMethod("POST");  
@@ -110,12 +142,15 @@ public class MyAsynctask extends AsyncTask<String, Integer, String>{
         	 flag=true;
          	 InputStream is=conn.getInputStream();
          	 json=StreamTools.readStream(is);
-         	
+         	 
           }else {
+        	InputStream is=conn.getInputStream();
+          	json=StreamTools.readStream(is);
         	flag=false;
-        	json="请求失败";
           }  
          } catch (Exception e) {
+        	 flag=false;
+        	 json=e.getMessage().toString();
  			e.printStackTrace();
  		}  
       }  
@@ -125,16 +160,26 @@ public class MyAsynctask extends AsyncTask<String, Integer, String>{
 	     String LINE_END = "\r\n"; 
          StringBuilder sb = new StringBuilder();  
      if(params!=null &params.size()!=0){  
-         for (Map.Entry<String, String> entry : params.entrySet()) {  
-         	  String value = entry.getValue();  
-              sb.append("--" + BOUNDARY + "\r\n");  
-              sb.append("Content-Disposition: form-data; name=\"" + entry.getKey()+"\"\r\n");  
-              sb.append("\r\n");  
-              sb.append(URLEncoder.encode(value) + "\r\n");
+    	Set<String> keys=params.keySet();
+     	Iterator<String> iter=keys.iterator();
+     	while(iter.hasNext()){
+     		String key=iter.next();
+     		if (!key.equals("path")&&!key.equals("type")&&!key.equals("isupfile")) {
+     			String value=params.get(key);
+     			sb.append("--" + BOUNDARY + "\r\n");  
+                sb.append("Content-Disposition: form-data; name=\"" + key+"\"\r\n");  
+                sb.append("\r\n");  
+                sb.append(URLEncoder.encode(value) + "\r\n");
+			}
+    	 
          }  
+     }
      byte[] entity = sb.toString().getBytes();  
      try {
-           URL url=new URL(path[0]);
+           URL url=new URL(params.get("path"));
+           File file =new File(params.get("isupfile"));
+     
+           Log.i(TAG, params.get("isupfile"));
            HttpURLConnection conn = (HttpURLConnection) url.openConnection();  
           conn.setDoOutput(true);  
           conn.setUseCaches(false);  
@@ -150,14 +195,14 @@ public class MyAsynctask extends AsyncTask<String, Integer, String>{
           buffer.append(PREFIX);    
           buffer.append(BOUNDARY); 
           buffer.append(LINE_END);    
-          buffer.append("Content-Disposition: form-data; name=\"" + name  
-                  + "\"; filename=\"" + URLEncoder.encode(file.getName()) + LINE_END);   
+          buffer.append("Content-Disposition: form-data; name=\"imgFile\"; filename=\"" + URLEncoder.encode(file.getName()) + LINE_END);   
           buffer.append("Content-Type: application/octet-stream; charset="+"UTF-8"+LINE_END);   
           //代表要上传的数据是文件类型application/octet-stream
           buffer.append(LINE_END);   
           //写请求头
-          dos.write(buffer.toString().getBytes());    
-          InputStream is = new FileInputStream(path[0]);   
+          dos.write(buffer.toString().getBytes());   
+         
+          InputStream is = context.getAssets().open(file.getName());   
           byte[] bytes = new byte[1024];    
           int len = 0;  
          // 写二进制数据文件
@@ -176,25 +221,26 @@ public class MyAsynctask extends AsyncTask<String, Integer, String>{
       System.out.println("返回码"+conn.getResponseCode());
       if (conn.getResponseCode() == 200) {  
      	InputStream is1 = conn.getInputStream();
-     	StringBuilder builder=new StringBuilder();
+		json=StreamTools.readStream(is1);
+     	/*StringBuilder builder=new StringBuilder();
  		BufferedReader reader=new BufferedReader(new InputStreamReader(is1));
  		String line=null;
  		while (((line=reader.readLine()))!=null) {
  			builder.append(line);
          }  
  		reader.close();
- 		json=reader.toString();
+ 		json=reader.toString();*/
  		flag=true;
  	
       }
      } catch (Exception e) {
     	 flag=false;
-    	 json="图片上传请求失败";
+    	 json="上传请求失败";
 		e.printStackTrace();
 		}  
      }
    
-  }
+  
          return json;  
 }  
     
